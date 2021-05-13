700ce0a3ee175b65e4f7f41c04843895b209d479
Idan Izicovich <idanizi@gmail.com>
2021-05-14 01:11:59 +0300

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import _config from './config'
-import apisauce from 'apisauce'
+import _config from './config';
+import apisauce from 'apisauce';
 
 const api = apisauce.create({
-  baseURL: "",
-})
+  baseURL: '',
+});
 
-const config = _config.get('app')
+const config = _config.get('app');
 
 function App() {
-
-  const [res, setRes]: [any, any] = React.useState("")
+  const [res, setRes]: [any, any] = React.useState('');
 
   React.useEffect(() => {
-    api.get('/api')
-    .then(res => {
-      if(res.ok){
-        setRes(res.data)
+    api.get('/api').then((res) => {
+      if (res.ok) {
+        setRes(res.data);
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div className="App">
       {config.test}
-      <div>
-        {'res: ' + res}
-      </div>
+      <div>{'res: ' + res}</div>
     </div>
   );
 }
--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -2,9 +2,11 @@ import _ from 'lodash';
 
 const config: any = {};
 config.app = {
-    test: 'this is poker game for talia'
-}
+  test: 'this is poker game for talia',
+};
 
-config.get = (path: string) => {return _.get(config, path)}
+config.get = (path: string) => {
+  return _.get(config, path);
+};
 
 export default config;
