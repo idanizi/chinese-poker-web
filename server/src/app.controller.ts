8dee053a8c42fea07a3f7ad4da35b8271e62b2b6
Idan Izicovich <idanizi@gmail.com>
2021-05-16 02:26:13 +0300

    app controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Hand } from 'pokersolver';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/hand/solve')
+  solve(@Body('cards') cards: string[]): any {
+    return Hand.solve(cards);
+  }
+
+  @Post('/hand/compare')
+  compare(@Body('hands') hands: string[][]): any {
+    const players = hands.map(cards => Hand.solve(cards));
+    const [winner, ] = Hand.winners(players)
+    const winner_index = players.findIndex(p => p === winner);
+    return { winner: {index: winner_index, name: winner?.name}, hands: players.map(p => ({name: p.name})) }
   }
 }
