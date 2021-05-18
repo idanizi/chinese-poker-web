eb98d1fe29c8cb60b53b3e4c2aee86d63f886ed8
Idan Izicovich <idan.izi@workiz.com>
2021-05-18 21:27:07 +0300

    fixme segment fault

--- client/src/redux/reducers/cards.ts
+++ client/src/redux/reducers/cards.ts
@@ -34,7 +34,7 @@ const cardsReducer = (state = initialState, action: FluxActionType<PayloadType>)
     switch (action.type) {
         case actions.ADD_CARD_TO_HAND:
             const player = action.payload.isCurrentPlayer ? 'currentPlayer' : 'oppositePlayer'
-            draft[player].hands[action.payload.handIndex].push(action.payload.cardName)
+            draft[player].hands[action.payload.handIndex].push(action.payload.cardName) // fixme: segment fault
             break;
 
         case actions.REMOVE_CARD_FROM_DECK:
